25d474e58d1151490439969623003790194cec12
Muhammad Miftah <mamift@gmail.com>
2021-05-28 10:01:26 +1000

    Add project files and example code.

---
+++ SimpleWfTest/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using WorkflowCore.Interface;
+using WorkflowCore.Models;
+using WorkflowCore.Models.LifeCycleEvents;
+using WorkflowCore.Services;
+
+namespace SimpleWfTest
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			await WfCoreMain();
+		}
+
+		private static async Task WfCoreMain()
+		{
+			IServiceProvider serviceProvider = ConfigureServices();
+
+			//start the workflow host
+			IWorkflowHost host = serviceProvider.GetService<IWorkflowHost>();
+			host.RegisterWorkflow<HelloWorldWorkflow>();
+			host.Start();
+
+			var hub = serviceProvider.GetService<ILifeCycleEventHub>();
+			var syncWorkflowExecUtility = new SynchronousWorkflowExecutionUtility(host, hub);
+
+			hub.Subscribe(@event => {
+				if (@event is WorkflowCompleted) {
+					Console.WriteLine($"{nameof(HelloWorldWorkflow)} is ending");
+				}
+			});
+
+			//var syncWfResult = await syncWorkflowExecUtility.StartWorkflowAndWait(nameof(HelloWorldWorkflow));
+			var id = await host.StartWorkflow(nameof(HelloWorldWorkflow));
+
+			//Console.WriteLine($"{nameof(HelloWorldWorkflow)} should've ended.");
+			Console.ReadLine();
+			host.Stop();
+		}
+
+		private static IServiceProvider ConfigureServices()
+		{
+			//setup dependency injection
+			IServiceCollection services = new ServiceCollection();
+			services.AddLogging();
+			services.AddWorkflow();
+			services.AddTransient<HelloWorldStep>();
+			services.AddTransient<GoodbyeWorldStep>();
+
+			var serviceProvider = services.BuildServiceProvider();
+
+			return serviceProvider;
+		}
+	}
+
+	class HelloWorldWorkflow : IWorkflow
+	{
+		public void Build(IWorkflowBuilder<object> builder)
+		{
+			IStepBuilder<object, HelloWorldStep> stepBuilder = builder.StartWith<HelloWorldStep>();
+			IStepBuilder<object, GoodbyeWorldStep> then = stepBuilder.Then<GoodbyeWorldStep>();
+			IStepBuilder<object, GoodbyeWorldStep> endWorkflow = then.EndWorkflow();
+		}
+
+		public string Id { get; } = nameof(HelloWorldWorkflow);
+		public int Version { get; } = 1;
+	}
+
+	class HelloWorldStep : StepBody
+	{
+		public override ExecutionResult Run(IStepExecutionContext context)
+		{
+			Console.WriteLine($"Hello world! {context.Workflow.Data}");
+			return ExecutionResult.Next();
+		}
+	}
+
+	class GoodbyeWorldStep : StepBody
+	{
+		public override ExecutionResult Run(IStepExecutionContext context)
+		{
+			Console.WriteLine($"Goodbye world!");
+			return ExecutionResult.Next();
+		}
+	}
+}
---
+++ SimpleWfTest/SynchronousWorkflowExecutionUtility.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WorkflowCore.Interface;
+using WorkflowCore.Models.LifeCycleEvents;
+
+namespace SimpleWfTest
+{
+	public class SynchronousWorkflowExecutionResult
+	{
+		public string WorkflowId { get; set; }
+		public string WorkflowInstanceId { get; set; }
+		public string Reference { get; set; }
+		public LifeCycleEvent LastLifeCycleEvent { get; set; }
+	}
+
+	public class SynchronousWorkflowExecutionUtility
+	{
+		private readonly IWorkflowHost _host;
+		private readonly ILifeCycleEventHub _hub;
+
+		private readonly Dictionary<string, TaskCompletionSource<SynchronousWorkflowExecutionResult>>
+			_completionSources = new Dictionary<string, TaskCompletionSource<SynchronousWorkflowExecutionResult>>();
+			
+		public SynchronousWorkflowExecutionUtility(IWorkflowHost host, ILifeCycleEventHub hub)
+		{
+			_host = host;
+
+			_hub = hub;
+			_hub.Subscribe(HandleWorkflowEvent);
+		}
+
+		private void HandleWorkflowEvent(LifeCycleEvent @event)
+		{
+			switch (@event) {
+				case WorkflowCompleted completed:
+				case WorkflowTerminated terminated:
+				case WorkflowError error:
+					if (_completionSources.ContainsKey(@event.WorkflowInstanceId)) {
+						var completionSource = _completionSources[@event.WorkflowInstanceId];
+						var result = (SynchronousWorkflowExecutionResult) completionSource.Task.AsyncState;
+						result.LastLifeCycleEvent = @event;
+						completionSource.SetResult(result);
+					}
+
+					break;
+			}
+		}
+
+		public async Task<SynchronousWorkflowExecutionResult> StartWorkflowAndWait(string workflowId,
+			int? version = null, object data = null, string reference = null) 
+		{
+			var result = new SynchronousWorkflowExecutionResult() {
+				WorkflowId = workflowId,
+				Reference = reference
+			};
+
+			var completionSource = new TaskCompletionSource<SynchronousWorkflowExecutionResult>(result);
+			var instanceId = await _host.StartWorkflow(workflowId, version, data, reference);
+			result.WorkflowInstanceId = instanceId;
+
+			_completionSources.Add(instanceId, completionSource);
+			return await completionSource.Task;
+		}
+	}
+}
